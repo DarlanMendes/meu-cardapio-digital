c31b3220bea6a7dc5e90c0862796ec701c459402
Darlan Mendes <dedsondarlan@gmail.com>
2023-07-25 23:33:09 -0300

    Ajuste e teste com adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,18 +15,19 @@ export const authOptions: NextAuthOptions = {
   ],
   secret: process.env.NEXT_PUBLIC_JWT_SECRET as string,
 
-  // adapter: FirestoreAdapter({
-  //   credential: cert({
-  //     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  //     clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
-  //     privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
-  //   }),
-  // }) as Adapter,
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+      privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+    }),
+  }) as Adapter,
   callbacks: {
     async session({ session, token }) {  
+      
       if (token ) {
         const firebaseToken = await admin.auth().createCustomToken(`${token!.jti}`)
-        //@ts-ignore
+      //@ts-ignore
         session.firebaseToken = firebaseToken
       }
       return session
