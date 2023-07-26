37dd028abc48083335fc798eebe4a7f0ce9e3657
Darlan Mendes <dedsondarlan@gmail.com>
2023-07-25 21:12:20 -0300

    Ajuste key index pges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ export default function Home(props: Props) {
       className={` ${playFairDisplay.className} `}
     >
       <div className=' grid gap-6 px-6 py-4 justify-center items-start grid-cols-1 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1'>
-        {props.tenants.map((tenant) => (
+        {props.tenants.map((tenant,index) => (
           <Link href={`/${tenant.slug}`}>
           
-          <div className='p-4 border-2 h-40 flex items-center w-full gap-2 bg-slate-100 rounded-md'>
+          <div className='p-4 border-2 h-40 flex items-center w-full gap-2 bg-slate-100 rounded-md' key={index}>
             <img src={tenant.logo} className='h-24 w-24 object-cover rounded-full border' />
             <div className='flex p-2 flex-col items-start justify-start h-full mt-8'>
                 <h1 className='text-2xl font-bold'> {tenant.name} </h1>
